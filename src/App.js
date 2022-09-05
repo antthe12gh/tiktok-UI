05bc1d787caa1d6230d3eadc29779e742f0d3c1a
antthe12gh <dinhthe2000gh@gmail.com>
2022-09-05 17:25:11 +0700

    prettier and css/sass

--- src/App.js
+++ src/App.js
@@ -4,33 +4,29 @@ import Home from './pages/Home';
 import News from './pages/News';
 import Contact from './pages/Contact';
 
-
 function App() {
+    return (
+        <div className="app">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
 
-  return (
-    <div className='app'>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/news'>News</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/news' element={<News/>}></Route>
-      <Route path='/contact' element={<Contact/>}></Route>
-    </Routes>
-    </div>
-      
-  )
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/news" element={<News />}></Route>
+                <Route path="/contact" element={<Contact />}></Route>
+            </Routes>
+        </div>
+    );
 }
 export default App;
